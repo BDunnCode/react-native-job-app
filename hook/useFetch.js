564cd4dae10bec69e7d2af91bdcd62e2745aad20
BDunnCode <BDunnCode@github.com>
2024-04-02 21:32:39 -0500

    add the fetch function for useFetch.js page

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,23 +1,42 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
 
-const useFetch = (endpoint) => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const rapidApiKey = RAPID_API_KEY;
 
 
-  const options = {
-    method: 'GET',
-    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
-    headers: {
-      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-    },
-    params: {
-      query: 'Python developer in Texas, USA',
-      page: '1',
-      num_pages: '1'
-    },
-  };
+    const options = {
+      method: 'GET',
+      url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+      headers: {
+        'X-RapidAPI-Key': rapidApiKey,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      },
+      params: {...query },
+    };
+
+    const fetchData = async () => {
+      setIsLoading(true);
+
+    try { 
+      const response = await axios.request(
+        options, 
+      );
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert('There is an error fetching the data.')
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  
 }
